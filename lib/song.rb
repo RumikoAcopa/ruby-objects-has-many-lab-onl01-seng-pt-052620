62228f332d08d465d4999f2af2c89e132db81a4a
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-23 03:41:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,10 +17,6 @@ class Song
     end
     
     def artist_name
-      if artist
-        self.artist.name
-      else
-        nil 
-      end
+        self.artist == ? == : self.artist.name
     end
 end
