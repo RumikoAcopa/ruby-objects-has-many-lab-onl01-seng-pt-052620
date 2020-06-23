78f15a8d080f33ce9fc459d92a766a0e8511a885
Rumiko Acopa Rios <rumikorios@gmail.com>
2020-06-23 03:24:28 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -13,9 +13,9 @@ class Author
       end
   end
   
-  def add_post(post)
-      post.author == self
-  end
+  # def add_post(post)
+  #     post.author == self
+  # end
   
   def dd_post_by_title(post_title)
         new_post = Post.new(post_title)
